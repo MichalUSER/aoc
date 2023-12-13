7b1136b9401b93279ebba643a9c41d6c2c9d7b2e
michqo <42279279+michqo@users.noreply.github.com>
2023-12-13 15:46:24 +0100

    feat: 2023 day1

---
+++ 2023/day1/src/first.rs
@@ -0,0 +1,14 @@
+/* fn main() {
+    let sum = include_str!("../input.txt").lines().fold(0, |acc, l| {
+        let sum2 = l.chars().fold(String::new(), |mut a, c| {
+            if c >= '\u{0030}' && c <= '\u{0039}' {
+                a.push(c)
+            }
+            a
+        });
+        let nums = sum2.chars().nth(0).unwrap().to_string()
+            + sum2.chars().last().unwrap().to_string().as_str();
+        acc + nums.parse::<i32>().unwrap()
+    });
+    println!("sum is {}", sum);
+} */
